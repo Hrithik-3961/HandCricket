bfa9078fb9943702369403e45f525ae8cbdfe6fb
Hrithik Agarwal <agarwal.hrithik3961@gmail.com>
2024-01-28 14:22:25 +0530

    Minor Changes

--- app/src/main/java/com/Hand_Cricket/HomeScreen.java
+++ app/src/main/java/com/Hand_Cricket/HomeScreen.java
@@ -1,7 +1,6 @@
 package com.Hand_Cricket;
 
 import android.app.AlarmManager;
-import android.app.Application;
 import android.app.PendingIntent;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
